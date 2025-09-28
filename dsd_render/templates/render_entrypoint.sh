441860ad62d732aeeaa994f846a7ca30d48b1e4b
Jake Atwell <jatwell@alexinc.com>
2025-09-27 23:22:37 -0400

    Rename settings file used in render_entrypoint.sh

--- dsd_render/templates/render_entrypoint.sh
+++ dsd_render/templates/render_entrypoint.sh
@@ -1,3 +1,3 @@
 python manage.py makemigrations
 python manage.py migrate
-DJANGO_SETTINGS_MODULE={{ django_project_name }}.settings_prod gunicorn blog.wsgi:application
+DJANGO_SETTINGS_MODULE={{ django_project_name }}.settings_render gunicorn blog.wsgi:application
